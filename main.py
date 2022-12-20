5b21d99e17a51e3638f8d086e628e4738681e0e1
Platon <platon.solovev05@gmail.com>
2022-12-20 19:04:12 +0300

    0.1.2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # перевод систем счисления
-def convert_base(num, from_base, to_base):
+def convert_base(num: int, from_base: int, to_base: int):
     n = int(str(num), from_base)
     alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     res = ""
@@ -10,48 +10,51 @@ def convert_base(num, from_base, to_base):
 
 
 # делители числа
-def dividers(a):
+def dividers(num: int):
     arr = []
-    for i in range(2, a):
-        if a % i == 0:
+    for i in range(2, num):
+        if num % i == 0:
             arr.append(i)
     return arr
 
 
 # оставляет уникальные значения массива
-def unique(numbers):
+def unique(num: str):
     unique = []
-    for number in numbers:
+    for number in num:
         if number not in unique:
             unique.append(number)
     return unique
 
 
 # проверка на простату
-def is_prime(num):
+def is_prime(num: int):
     d = 2
     while d * d <= num and num % d != 0:
         d += 1
     return d * d > num
 
 
-# нечетные цифры числа
-def odd(a):
-    a = int(a)
+# количество нечетных цифр числа
+def odd(num: int):
     odd = 0
-    while a > 0:
-        if a % 2 != 0:
+    while num > 0:
+        if num % 2 != 0:
             odd += 1
-        a = a // 10
+        num = num // 10
     return odd
 
 
-# четные цифры числа
-def even(a):
-    a = int(a)
+# количество четных цифр числа
+def even(num: int):
     even = 0
-    while a > 0:
-        if a % 2 == 0:
+    while num > 0:
+        if num % 2 == 0:
             even += 1
-        a = a // 10
+        num = num // 10
     return even
+
+
+# сумма цифр числа
+def summa(num: str):
+    return sum(map(int, str(num)))
